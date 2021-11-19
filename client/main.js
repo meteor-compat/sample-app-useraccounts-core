c61c669a4cca95cd1cd1c8a1ade4b1220790c6a0
filipenevola <filipemkv@gmail.com>
2021-11-19 12:31:37 -0400

    Update sample with Meteor 2.5.1 and other dependencies like iron:router

--- client/main.js
+++ client/main.js
@@ -1,7 +1,22 @@
+import { Meteor } from 'meteor/meteor';
 import {Template} from 'meteor/templating';
 import {ReactiveVar} from 'meteor/reactive-var';
+import {Router} from 'meteor/iron:router';
+import {AccountsTemplates} from 'meteor/accounts-base';
 
 import './main.html';
+
+Router.configure({
+  layoutTemplate: 'masterLayout',
+  yieldTemplates: {
+    myNav: {to: 'nav'},
+    myFooter: {to: 'footer'},
+  }
+});
+
+AccountsTemplates.configure({
+  defaultLayout: 'masterLayout',
+});
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
